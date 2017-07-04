72538907c5f0b3c8be3678c6e1f77df8da143fc6
maryfranks <maryfranks@gmail.com>
2017-07-04 14:49:14 -0400

    adds initialize, balance reader, withdraw and deposit methods

--- bank_account.rb
+++ bank_account.rb
@@ -1,7 +1,24 @@
 class BankAccount
 
-@@interest_rate = 0.0
-@@accounts = []
+  @@interest_rate = 0.0
+  @@accounts = []
 
+def initialize
+  @balance = 0
+end
+
+def get_balance
+  return @balance
+end
+
+def deposit(amount)
+  @balance = @balance + amount
+  return @balance
+end
+
+def withdrawl(amount)
+  @balance = @balance - amount
+  return @balance
+end
 
 end
