d54c4e6255a6b59a5d4574290c14d88dd0067d84
maryfranks <maryfranks@gmail.com>
2017-07-04 15:18:00 -0400

    created create and show accounts class methods

--- bank_account.rb
+++ bank_account.rb
@@ -3,22 +3,30 @@ class BankAccount
   @@interest_rate = 0.0
   @@accounts = []
 
-def initialize
-  @balance = 0
-end
+  def self.create
+    @@accounts << BankAccount.new
+  end
 
-def get_balance
-  return @balance
-end
+  def self.show_accounts
+    return @@accounts
+  end
 
-def deposit(amount)
-  @balance = @balance + amount
-  return @balance
-end
+  def initialize
+    @balance = 0
+  end
 
-def withdrawl(amount)
-  @balance = @balance - amount
-  return @balance
-end
+  def get_balance
+    return @balance
+  end
+
+  def deposit(amount)
+    @balance = @balance + amount
+    return @balance
+  end
+
+  def withdrawl(amount)
+    @balance = @balance - amount
+    return @balance
+  end
 
 end
