e5356b0575bdde26474b6ea0b9efb4310876eeec
maryfranks <maryfranks@gmail.com>
2017-07-04 19:18:44 -0400

    adds due date methods

--- book_lending.rb
+++ book_lending.rb
@@ -23,31 +23,46 @@ class Book
     available_books.sample
   end
 
+  def self.current_due_date
+    @due_date = Time.now + 604800
+    return @due_date
+  end
+
 # ======== Instance =========
 
   def initialize(title, author, isbn)
     @@author = author
     @@title = title
     @@isbn = isbn
+    @borrowed = false
+    @due_date = false
+  end
+
+  def lent_out?
+    if @borrowed == true
+      return true
+    else
+      return false
+    end
   end
 
   def get_due_date
-
+    return @due_date
   end
 
   def set_due_date
-
+    @due_date = Time.now + 604800
   end
 
 
 end
 
-
+# 604800 seconds in a week
 # ===== For Testing =====
 book1 = Book.create("The Bone Clocks", "David Mitchell", 12345)
 book2 = Book.create("The Left Hand of Darkness", "Ursula Le Guin", 54321)
 book3 = Book.create("The Winter People", "Jennifer McMahon", 63748)
 book4 = Book.create("Cloud Atlas", "David Mitchell", 35472)
 
-# puts book1
-puts Book.browse
+
+puts Book.current_due_date
