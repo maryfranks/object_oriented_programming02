5dcb58a4f174b44eb295750de25672945775558c
maryfranks <maryfranks@gmail.com>
2017-07-04 17:23:22 -0400

    adds initialize method with title, author, and isbn

--- book_lending.rb
+++ book_lending.rb
@@ -3,7 +3,22 @@ class Book
   @@on_shelf = []
   @@on_loan = []
 
-  
+  def initialize(title, author, isbn)
+    @@author = author
+    @@title = title
+    @@isbn = isbn
+  end
+
+  def get_due_date
+
+  end
+
+  def set_due_date
+
+  end
 
 
 end
+
+
+book1 = Book.new("Title", "Author", 12345)
