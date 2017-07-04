4e162a4fdab678fda7d81dce90ddb543a3ca5b60
maryfranks <maryfranks@gmail.com>
2017-07-04 18:34:41 -0400

    adds class method browse

--- book_lending.rb
+++ book_lending.rb
@@ -11,7 +11,16 @@ class Book
   end
 
   def self.available
-    return @@on_shelf 
+    return @@on_shelf
+  end
+
+  def self.borrowed
+    return @@on_loan
+  end
+
+  def self.browse
+    available_books = @@on_shelf
+    available_books.sample
   end
 
 # ======== Instance =========
@@ -35,5 +44,10 @@ end
 
 
 # ===== For Testing =====
-book1 = Book.create("Title", "Author", 12345)
-puts book1
+book1 = Book.create("The Bone Clocks", "David Mitchell", 12345)
+book2 = Book.create("The Left Hand of Darkness", "Ursula Le Guin", 54321)
+book3 = Book.create("The Winter People", "Jennifer McMahon", 63748)
+book4 = Book.create("Cloud Atlas", "David Mitchell", 35472)
+
+# puts book1
+puts Book.browse
