86e4f841e0d04f09562c172c47db10ffa145d199
maryfranks <maryfranks@gmail.com>
2017-07-05 19:13:28 -0400

    adds empty methods to zombies.rb

--- zombies.rb
+++ zombies.rb
@@ -2,15 +2,51 @@ class Zombies
 
 # ======== Class =========
 
+  @@horde = []
+  @@plague_level = 10
+  @@max_speed = 5
+  @@max_strength = 8
+  @@default_speed = 1
+  @@default_strength = 3
 
+  def self.all
 
+  end
+
+  def self.new_day
+
+  end
+
+  def self.some_die_off
+
+  end
+
+  def self.spawn
+
+  end
+
+  def self.increase_plague_level
+
+  end
 
 # ======== Instance =========
 
+  def initialize
 
+  end
 
+  def encounter
+
+  end
+
+  def outrun_zombie
+
+  end
+
+  def survive_attack
+
+  end
 
 end
-
 
 # ======== Testing =========
