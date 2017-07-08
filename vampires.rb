9166bdfeec9141f1870d955c79c8e97442aaba92
maryfranks <maryfranks@gmail.com>
2017-07-08 19:34:18 -0400

    adds sunset method to vampires.rb

--- vampires.rb
+++ vampires.rb
@@ -1,23 +1,44 @@
 class Vampire
 
+  attr_accessor :in_coffin, :drank_blood_today
+
   @@coven = []
 
 # ======================= Initialize =======================
 
   def initialize(name, age)
-    @@name              = name
-    @@age               = age
-    @@in_coffin         = false
-    @@drank_blood_today = false
+    @name              = name
+    @age               = age
+    @in_coffin         = false
+    @drank_blood_today = false
   end
 
 # ===================== Class Methods ======================
 
-  def self.create
-    @@coven << Vampire.new
-    return @@coven.last 
+  def self.create(name, age)
+    @@coven << Vampire.new(name, age)
+    return @@coven.last
+  end
+
+  def self.all
+    @@coven
+  end
+
+  def self.sunset
+    @@coven.each do |vampire|
+      vampire.drank_blood_today = false
+      vampire.in_coffin = false
+    end
   end
 
 # ==================== Instance Methods =====================
+
+  def drink_blood
+    @drank_blood_today = true
+  end
+
+  def go_home
+    @in_coffin = true
+  end
 
 end
